b699eff5f2253d68f0ec164aa54177c5163e9606
Guru Teja <tejachowdhary121@gmail.com>
2024-08-14 23:00:31 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,9 +39,9 @@ $(document).ready(function () {
 
     // <!-- emailjs to mail contact form data -->
     $("#contact-form").submit(function (event) {
-        emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
+        emailjs.init("eFvd1M7EcMXu4nRFu");
 
-        emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
+        emailjs.sendForm('service_zkcc0ez', 'template_evvmje5', '#contact-form')
             .then(function (response) {
                 console.log('SUCCESS!', response.status, response.text);
                 document.getElementById("contact-form").reset();
